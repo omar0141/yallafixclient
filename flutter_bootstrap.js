fbd1cd0134a46247c6c2a32ca6fac9ef3c4e7175
omar0141 <75172714+omar0141@users.noreply.github.com>
2026-07-02 00:09:45 +0000

    Deploying to main from @ omar0141/yallafix_client@737713da88eab1ef92a2685751cde8a0895bb042 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"a10d8ac38de835021c8d2f920dbf50a920ccc0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4176676173" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3731247717" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
